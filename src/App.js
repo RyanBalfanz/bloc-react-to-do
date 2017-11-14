c1903fafba174c48e33338ba33c431305ed280e3
Ryan Balfanz <rbalfanz@gmail.com>
2017-11-13 23:25:07 -0800

    Import the ToDo Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ul>
+          <ToDo />
+          <ToDo />
+        </ul>
       </div>
     );
   }
