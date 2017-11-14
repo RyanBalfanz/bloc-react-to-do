c4ceafda0ea8e9da513ae19ad3d325cb72bfe977
Ryan Balfanz <rbalfanz@gmail.com>
2017-11-13 23:34:02 -0800

    Rendering the isCompleted prop as checkbox input
    ```
    index.js:2177 Warning: Failed prop type: You provided a `checked` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultChecked`. Otherwise, set either `onChange` or `readOnly`.
        in input (at ToDo.js:7)
        in li (at ToDo.js:6)
        in ToDo (at App.js:22)
        in ul (at App.js:20)
        in div (at App.js:19)
        in App (at index.js:7)
    ```

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class ToDo extends Component {
     render() {
         return (
-            <li>{ this.props.description }</li>
+        <li>
+            <input type="checkbox" checked={ this.props.isCompleted } />
+            <span>{ this.props.description }</span>
+        </li>
         );
       }
 }
