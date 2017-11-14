c6903a2be4767da5601ea22bb6fff67cbdf17c16
Ryan Balfanz <rbalfanz@gmail.com>
2017-11-14 00:24:14 -0800

    Pass the deleteTodo method to the ToDo component as a prop

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
       <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) => 
-          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) } />
         )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
