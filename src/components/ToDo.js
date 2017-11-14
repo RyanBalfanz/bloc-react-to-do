24bb50b7cf35bd36064fd4c9939790b5afa3a0d1
Ryan Balfanz <rbalfanz@gmail.com>
2017-11-13 23:31:10 -0800

    Render the description prop in our ToDo component

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ToDo extends Component {
     render() {
         return (
-          <li> A todo will go here </li>
+            <li>{ this.props.description }</li>
         );
       }
 }
